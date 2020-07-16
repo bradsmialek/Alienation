de4e7a41061272be239bcfc14e71357338c5288e
bradsmialek <bradsmilek@gmail.com>
2020-07-16 12:46:55 -0700

    tests skeleton

---
+++ tests/com/alienation/game/InventoryTest.java
@@ -0,0 +1,20 @@
+package com.alienation.game;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by bradsmialek on Thu - 7/16/20 @ 12:45 PM
+ */
+public class InventoryTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
