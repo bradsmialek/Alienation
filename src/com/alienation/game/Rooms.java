4ee9f1ecad3e253dd6a70f036c8986ca6d5fcb7c
Dhruti Kosta <koshtidhruti@gmail.com>
2020-07-17 17:20:52 -0500

    Added Directions functionality

--- src/com/alienation/game/Rooms.java
+++ src/com/alienation/game/Rooms.java
@@ -1,25 +1,9 @@
 package com.alienation.game;
 
-/**
- * Rooms
- */
-public class Rooms{
-
-//    String[] room = {
-
-
-
-//        "Kitchen":{
-//        'north':'Hall',
-//        'item':'monster',
-//        },
-//        'Dining Room':{
-//        'west':'Hall',
-//        'south':'Garden',
-//        'item':'potion'
-//        },
-//        'Garden':{
-//        'north':'Dining Room'
-//        }
-
+public enum Rooms {
+    CapsuleRoom,
+    AlienRoom,
+    Kitchen,
+    ComputerRoom,
+    ControlRoom
 }
