0561d434eac6d2e96de3d270e1572f3251df5b4c
Dhruti Kosta <koshtidhruti@gmail.com>
2020-07-17 14:13:39 -0500

    Created Rooms

--- src/com/alienation/game/Engine.java
+++ src/com/alienation/game/Engine.java
@@ -37,23 +37,8 @@ public class Engine {
                 "██║░░██║███████╗██║███████╗██║░╚███║██║░░██║░░░██║░░░██║╚█████╔╝██║░╚███║\n" +
                 "╚═╝░░╚═╝╚══════╝╚═╝╚══════╝╚═╝░░╚══╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░╚════╝░╚═╝░░╚══╝\n\n");
 
-        System.out.println("As you open your eyes your vision is blurry and your body hurts. You gasp to take your first breath as you wake from cryo-sleep.\n" +
-                "You can tell the oxygen levels are low as it seems harder to breathe.  As you look around you notice that there is one crew member missing and\n" +
-                "their sleeping capsule is shattered. The ship seems to be drifting in space and the lights are dim, most likely on some sort of backup system.\n" +
-                "You notice a taser on the floor.\n\n" +
-                actionQuestion + "              " + "Health: 100" + "  " + "Oxygen: 50" + "  " + "Weapon: " + "\n");
-
-        System.out.println("You can: Investigate, Open, Eat, Pick up, Attack, Read, Swap");
-        System.out.println("You can move: N, S, E, W");
+        CapsuleRoom.loadEnvironment();
         actionQuestion = in.nextLine();
         System.out.println(actionQuestion);
-
-
-
-
-
-        in.close(); // CLOSES SCANNER
     }
-
-
 }
