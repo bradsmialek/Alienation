9503f93f3982f540dbd180a92de2cb490b690a29
bradsmialek <bradsmilek@gmail.com>
2020-07-24 13:38:33 -0700

    unit tests for grab

--- tests/com/alienation/game/MenuTest.java
+++ tests/com/alienation/game/MenuTest.java
@@ -1,6 +1,11 @@
 package com.alienation.game;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 import static org.junit.Assert.*;
 
@@ -8,6 +13,12 @@ import static org.junit.Assert.*;
  * Created by bradsmialek on Fri - 7/24/20 @ 12:26 PM
  */
 public class MenuTest {
+    String item;
+
+    @Before
+    public void init(){
+        Scanner in = new Scanner(System.in);
+    }
 
     @Test
     public void swap() {
@@ -42,7 +53,52 @@ public class MenuTest {
     }
 
     @Test
-    public void grab() {
+    //happy
+    public void grab_anItem_should_storeIn_inventory() throws FileNotFoundException {
+        File text = new File("/Users/bradsmialek/tlg/java/projects/Alienation/src/grabtoinventory.txt");
+        Scanner scnr = new Scanner(text);
+        System.out.println(scnr);
+        System.out.println("passed");
+    }
+    @Test
+    //fail
+    public void grab_an_Inelligible_itemShould_throwError() throws FileNotFoundException {
+        File text = new File("/Users/bradsmialek/tlg/java/projects/Alienation/src/grabFail.txt");
+        Scanner scnr = new Scanner(text);
+        System.out.println(scnr);
+        System.out.println("passed");
+    }
+
+    @Test
+    //happy oxy
+    public void grabOxygenTank_shouldIncrease_o2Levels() throws FileNotFoundException{
+        File text = new File("/Users/bradsmialek/tlg/java/projects/Alienation/src/grabOxygen.txt");
+        Scanner scnr = new Scanner(text);
+        int lineNumber = 1;
+        while(scnr.hasNextLine()){
+            String line = scnr.nextLine();
+            System.out.println("line " + lineNumber + " :" + line);
+            lineNumber++;
+            System.out.println(line.getClass());
+            System.out.println();
+        }
+    }
+    @Test
+    //fail oxy
+    public void grabOxygenTank_should_NOT_Increase_o2Levels() throws FileNotFoundException{
+        File text = new File("/Users/bradsmialek/tlg/java/projects/Alienation/src/grabOxygen.txt");
+        Scanner scnr = new Scanner(text);
+        int lineNumber = 1;
+        while(scnr.hasNextLine()){
+            String line = scnr.nextLine();
+            System.out.println("line " + lineNumber + " :" + line);
+            lineNumber++;
+            System.out.println(line.getClass());
+            System.out.println();
+        }
+
+        //getOxygen()
+
     }
 
     @Test
