a5b19229497926dc9994fbdcee0524f741844159
BruceBAWest <brucebawest@gmail.com>
2020-07-30 12:41:17 -0700

    added comment heading to all files with repo and contributor information

--- src/com/alienation/coregamefiles/Menu.java
+++ src/com/alienation/coregamefiles/Menu.java
@@ -1,3 +1,25 @@
+/*
+ *      Alienation
+ *      TLG Learning: Capstone Project
+ *      Originally Created by Team Alienation &&
+ *      Edited by Team Capstone Mafia
+ *      https://github.com/Capstone-Mafia
+ *
+ *      Team Alienation Members:
+ *      Brad Smialek (https://github.com/bradsmialek)
+ *      Dhruti Kosta (https://github.com/dhruti-kosta)
+ *      Terrell Douglas (https://github.com/Dougie105)
+ *      Original project repo:
+ *      https://github.com/bradsmialek/Alienation
+ *
+ *      Capstone Mafia Members:
+ *      Bruce West (https://github.com/BruceBAWest)
+ *      Gurinder Batth (https://github.com/GurinderB)
+ *      Daeun Lok (https://github.com/koreareefclub)
+ *      Capstone Mafia fork:
+ *      https://github.com/Capstone-Mafia/Alienation
+ */
+
 package com.alienation.coregamefiles;
 
 import com.alienation.enginefiles.Engine;
